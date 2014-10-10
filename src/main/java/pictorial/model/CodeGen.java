00aa100738b317307a4b781c56579204f3712788
Scheiber <scheiber@eyesopen.com>
2014-10-10 10:26:45 -0600

    load the templates once instead of every time the image is generated

--- src/main/java/pictorial/model/CodeGen.java
+++ src/main/java/pictorial/model/CodeGen.java
@@ -9,30 +9,54 @@ import javafx.stage.FileChooser;
 import java.io.*;
 
 public class CodeGen {
-    private static String generateCode(String extension, Settings.LanguageFormat langFormat, Settings settings) throws IOException, TemplateException {
+    private static Template _pythonTemplate;
+    private static Template _javaTemplate;
+    private static Template _cppTemplate;
+    private static Template _csTemplate;
+
+    static {
         Configuration cfg = new Configuration();
         cfg.setClassForTemplateLoading(System.class, "/pictorial");
         cfg.setObjectWrapper(new DefaultObjectWrapper());
         cfg.setDefaultEncoding("UTF-8");
-        Template temp = cfg.getTemplate("template" + extension);
+        try {
+            _pythonTemplate = cfg.getTemplate("template.py");
+            _csTemplate = cfg.getTemplate("template.cs");
+            _cppTemplate = cfg.getTemplate("template.cpp");
+            _javaTemplate = cfg.getTemplate("template.java");
+        } catch(Exception e) {
+            System.out.print("Failed to load a template: " + e.getMessage());
+            System.exit(1);
+        }
+    }
+
+    private static String generateCode(String language, Settings.LanguageFormat langFormat, Settings settings) throws IOException, TemplateException {
         StringWriter sw = new StringWriter();
-        temp.process(settings.getHashTable(langFormat), sw);
-        return sw.toString();
+        if (language == "python")
+            _pythonTemplate.process(settings.getHashTable(langFormat), sw);
+        else if (language == "java")
+            _javaTemplate.process(settings.getHashTable(langFormat), sw);
+        else if (language == "c++")
+            _cppTemplate.process(settings.getHashTable(langFormat), sw);
+        else if (language == "csharp")
+            _cppTemplate.process(settings.getHashTable(langFormat), sw);
+
+            return sw.toString();
     }
 
     public static String generateCpp(Settings settings) throws IOException, TemplateException {
         Settings.LanguageFormat cppFormat = s -> s;
-        return CodeGen.generateCode(".cpp", cppFormat, settings);
+        return CodeGen.generateCode("c++", cppFormat, settings);
     }
 
     public static String generateJava(Settings settings) throws IOException, TemplateException {
         Settings.LanguageFormat javaFormat = s -> s.replace("::", ".");
-        return CodeGen.generateCode(".java", javaFormat, settings);
+        return CodeGen.generateCode("java", javaFormat, settings);
     }
 
     public static String generateCSharp(Settings settings) throws IOException, TemplateException {
         Settings.LanguageFormat csFormat = s -> s.replace("::", ".");
-        return generateCode(".cs", csFormat, settings);
+        return generateCode("csharp", csFormat, settings);
     }
 
     public static String generatePython(Settings settings) throws IOException, TemplateException {
@@ -44,6 +68,6 @@ public class CodeGen {
             }
             return s.replace("::", "_");
         };
-        return generateCode(".py", pyFormat, settings);
+        return generateCode("python", pyFormat, settings);
     }
 }
